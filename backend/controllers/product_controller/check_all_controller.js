b56a9d12cf740e262d45bc7d041287eb5b1e1b6f
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-11 22:26:41 +0530

    write business logic for checking all products

--- backend/controllers/product_controller/check_all_controller.js
+++ backend/controllers/product_controller/check_all_controller.js
@@ -1,3 +1,19 @@
-export const checkAllController = (req, res)=>{
-    console.log("check controller")
+import { ProductModel } from "../../models/Product.js"
+
+export const checkAllController = async(req, res)=>{
+    try {
+        const allProduct = await ProductModel.find()
+
+        return res.status(200).json({
+            message : "check all products",
+            status : true,
+            allProduct
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : "server error in get product controller.",
+            status : true,
+            error
+        })
+    }
 }
