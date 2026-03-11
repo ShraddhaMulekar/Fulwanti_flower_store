242551b6fd47b22a229847b080d6274d9c7d6637
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-12 01:40:12 +0530

    fix the bug on order router

--- backend/router/order_router.js
+++ backend/router/order_router.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { createOrderController } from "../controllers/order_controller/create_order_controller.js"
 import { getOrdersController } from "../controllers/order_controller/get_orders_controller.js"
+import { protect } from "../middleware/authMiddleware.js"
 
 export const orderRouter = express.Router()
 
-orderRouter.post("/create", createOrderController)
-orderRouter.get("/check", getOrdersController)
+orderRouter.post("/create", protect, createOrderController)
+orderRouter.get("/check", protect, getOrdersController)
