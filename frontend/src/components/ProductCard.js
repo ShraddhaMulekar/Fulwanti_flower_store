42e677b122e5ffbbec01603f2dd77210d99ce477
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-13 17:59:13 +0530

    product cart created using useDispatch hook

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>₹{product.price}</p>
+      <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
