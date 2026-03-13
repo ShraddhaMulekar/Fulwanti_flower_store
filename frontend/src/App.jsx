4ace535f5d8d8901ba42ea68203260269be84167
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-13 15:10:40 +0530

    install react router dom and create routes and route for specific home, product, cart pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
 
 function App() {
 
   return (
-    <>
-      flower
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
