5793196f2061d9aed301062bb317afa2fd16546c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-13 17:42:54 +0530

    cartslice reducer logic created as well created the add-to-card & remove-cart logic. import cartReducer from cartSlice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,6 @@
 import configureStore from "@reduxjs/toolkit";
 import productReducer from "../redux/productSlice";
+import cartReducer from "../redux/cartSlice"
 
 export const store = configureStore({
   reducer: {
