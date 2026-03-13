b37e716d2b5c3261f7689d93069f67be9f24d3fb
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-13 17:49:59 +0530

    navbar logic creted

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,4 +1,4 @@
-import configureStore from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import productReducer from "../redux/productSlice";
 import cartReducer from "../redux/cartSlice"
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <h2>Fulwanti Flower Shop</h2>
+
+      <Link to="/">Home</Link>
+      <Link to="/products">Products</Link>
+      <Link to="/cart">Cart</Link>
+      <Link to="/admin">Admin</Link>
+    </div>
   )
 }
 
