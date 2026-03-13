fe3d96a2d2af9b8de24e5775d066f1334e75108a
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-13 21:34:17 +0530

    create filter logic

---
+++ frontend/src/components/FilterBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FilterBar = ({setFilter}) => {
+
+  return (
+    <select onChange={(e)=>setFilter(e.target.value)}>
+        <option value="">All</option>
+        <option value="rose">Rose</option>
+        <option value="tulip">Tulip</option>
+    </select>
+  )
+}
+
+export default FilterBar
