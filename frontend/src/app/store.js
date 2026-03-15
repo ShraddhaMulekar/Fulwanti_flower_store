ae668710a4f0d409eb9ada4f6f459744774f1086
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-15 15:32:57 +0530

    authReducer connected with store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import productReducer from "../redux/productSlice";
 import cartReducer from "../redux/cartSlice"
+import authReducer from "../redux/authSlice"
 
 export const store = configureStore({
   reducer: {
+    auth : authReducer,
     products: productReducer,
     cart: cartReducer,
   },
